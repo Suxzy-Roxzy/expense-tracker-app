381343f52ce1fd744ea962768306b0d46df2e8db
Abani Success <successabani@gmail.com>
2025-11-11 18:11:59 -0800

    initial commit message

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,19 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Jost } from "next/font/google";
+// import {CenturyGothic} from "next/font/"
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const jost = Jost({
+  variable: "--font-jost",
   subsets: ["latin"],
+  
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+  weight: ["400"],
 });
 
 export const metadata: Metadata = {
@@ -25,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${jost.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
